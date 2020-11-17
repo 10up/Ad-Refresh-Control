7b66e1399838916b7f03320b006cc1ddf5486185
David Green <coding@davidrg.com>
2020-11-17 11:28:30 -0600

    Change Webpack config to use CoreJS 3 instead of 2 to handle ES7 polyfills

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -72,7 +72,7 @@ module.exports = {
 								[ '@babel/preset-env',
 									{
 										'useBuiltIns': 'usage',
-										'corejs': 2,
+										'corejs': 3,
 									} ]
 							],
 							cacheDirectory: true,
