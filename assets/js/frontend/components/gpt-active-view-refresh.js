010cbb2ddfc89c03212ee37706984c00f7a9fbfb
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2024-06-18 18:17:50 +0530

    fix js lint errors

--- assets/js/frontend/components/gpt-active-view-refresh.js
+++ assets/js/frontend/components/gpt-active-view-refresh.js
@@ -1,10 +1,10 @@
-const {googletag} = window;
+const { googletag } = window;
 const advertiserIds = window.AdRefreshControl.advertiserIds || []; // Do not trigger active view refresh for the given advertiserId.
 const viewabilityThreshold = window.AdRefreshControl.viewabilityThreshold || 70; // Percentage of visibility above which to trigger active view refresh.
 const lineItemIds = window.AdRefreshControl.lineItemIds || []; // Do not trigger active view refresh for the given line item Ids.
 const sizesToExclude = window.AdRefreshControl.sizesToExclude || []; // Do not trigger active view refresh for the given sizes.
 const slotIdsToExclude = window.AdRefreshControl.slotIdsToExclude || []; // Do not trigger active view refresh for the given slot IDs.
-const refreshInterval      = ( window.AdRefreshControl.refreshInterval || 30 ) * 1000;
+const refreshInterval = (window.AdRefreshControl.refreshInterval || 30) * 1000;
 const maximumRefreshes = window.AdRefreshControl.maximumRefreshes || 10;
 const refreshCallback = window.AdRefreshControl.refreshCallback || false;
 let browserFocus = true;
@@ -16,119 +16,38 @@ const adsData = [];
 const checkForAdsReadyToRefresh = () => {
 	const slotsToRefresh = [];
 
-	if ( ! browserFocus ) {
-		setTimeout( checkForAdsReadyToRefresh, 500 );
+	if (!browserFocus) {
+		setTimeout(checkForAdsReadyToRefresh, 500);
 		return;
 	}
-	for ( const slot in adsData ) {
-		if ( adsData.hasOwnProperty( slot ) ) {
-			if ( ! adsData[ slot ].canRefresh || ! adsData[ slot ].viewable ||
-					adsData[ slot ].viewability < viewabilityThreshold ) {
+	for (const slot in adsData) {
+		if (adsData.hasOwnProperty(slot)) {
+			if (
+				!adsData[slot].canRefresh ||
+				!adsData[slot].viewable ||
+				adsData[slot].viewability < viewabilityThreshold
+			) {
 				continue;
 			}
-			adsData[ slot ].timeViewable += 500;
-			if ( adsData[ slot ].timeViewable >= refreshInterval ) {
-				slotsToRefresh.push( adsData[ slot ].slotObject );
+			adsData[slot].timeViewable += 500;
+			if (adsData[slot].timeViewable >= refreshInterval) {
+				slotsToRefresh.push(adsData[slot].slotObject);
 				// Reset the time viewable to prevent a double refresh if the
 				// rendering is slow.
-				adsData[ slot ].timeViewable = 0;
+				adsData[slot].timeViewable = 0;
 			}
 		}
 	}
 
-	if ( 0 < slotsToRefresh.length ) {
-		if ( refreshCallback && 'function' === typeof window[refreshCallback] ) {
-			window[refreshCallback]( slotsToRefresh );
+	if (slotsToRefresh.length > 0) {
+		if (refreshCallback && typeof window[refreshCallback] === 'function') {
+			window[refreshCallback](slotsToRefresh);
 		} else {
-			googletag.pubads().refresh( slotsToRefresh );
+			googletag.pubads().refresh(slotsToRefresh);
 		}
 	}
 
-	setTimeout( checkForAdsReadyToRefresh, 500 );
-};
-
-/**
- * Handle impressionViewable event.
- * @param {object} event googletag.events.ImpressionViewableEvent
- * see: https://developers.google.com/doubleclick-gpt/reference#googletag.events.impressionviewableevent
- */
-const impressionViewableHandler = ( event ) => {
-	const slotID = event.slot.getSlotElementId();
-	if ( ! slotID ) {
-		return;
-	}
-	if ( 'undefined' === typeof adsData[ slotID ] ) {
-		initializeSlotData( event );
-	}
-	adsData[ slotID ].viewable = true;
-};
-
-
-/**
- * Handle slotVisibilityChanged event.
- * @param {object} event googletag.events.SlotVisibilityChangedEvent
- * see: https://developers.google.com/doubleclick-gpt/reference#googletageventsslotvisibilitychangedevent
- */
-const viewabilityHandler = ( event ) => {
-	let {inViewPercentage} = event;
-
-	if ( 'undefined' === typeof event.inViewPercentage ) {
-		inViewPercentage = 100;
-	}
-
-	const slotID = event.slot.getSlotElementId();
-	if ( 'undefined' === typeof adsData[ slotID ] ) {
-		initializeSlotData( event );
-	}
-
-	adsData[ slotID ].viewability = inViewPercentage;
-};
-
-/**
- * Handle slotRenderEnded event.
- * @param {object} event googletag.events.SlotRenderEndedEvent
- * see: https://developers.google.com/doubleclick-gpt/reference#googletag.events.slotrenderendedevent
- */
-const slotRenderEndedHandler = ( event ) => {
-	const slotID = event.slot.getSlotElementId();
-	if ( ! slotID ) {
-		return;
-	}
-	if ( 'undefined' === typeof adsData[ slotID ] ) {
-		initializeSlotData( event );
-	} else {
-		adsData[ slotID ].renderCount += 1;
-		adsData[ slotID ].timeViewable = 0;
-		adsData[ slotID ].viewable = false;
-		adsData[ slotID ].canRefresh = isEligible( event );
-	}
-};
-
-/**
- * Initialize the data being recorded for the slot.
- * @param {object} event googletag.events.SlotRenderEndedEvent
- * see: https://developers.google.com/doubleclick-gpt/reference#googletag.events.slotrenderendedevent
- */
-const initializeSlotData = ( event ) => {
-	const {slot} = event;
-	const slotID = slot.getSlotElementId();
-	if ( ! slotID ) {
-		return;
-	}
-
-	if ( 'undefined' !== typeof adsData[ slotID ] ) {
-		return;
-	}
-
-	adsData[ slotID ] = {};
-	adsData[ slotID ].viewable = true;
-	// In the event the ad rendered before the plugin's event handlers were
-	// added, we need to ensure the ad data is fully populated.
-	adsData[ slotID ].renderCount = 1;
-	adsData[ slotID ].timeViewable = 0;
-	adsData[ slotID ].canRefresh = isEligible( event );
-	adsData[ slotID ].slotObject = slot;
-	adsData[ slotID ].viewability = 0;
+	setTimeout(checkForAdsReadyToRefresh, 500);
 };
 
 /**
@@ -137,10 +56,10 @@ const initializeSlotData = ( event ) => {
  * see: https://developers.google.com/doubleclick-gpt/reference#googletag.events.slotrenderendedevent
  * @returns bool
  */
-const isEligible = ( event ) => {
-	const {slot} = event;
+const isEligible = (event) => {
+	const { slot } = event;
 	const slotInfo = slot.getResponseInformation();
-	if ( ! slotInfo ) {
+	if (!slotInfo) {
 		return false;
 	}
 
@@ -151,33 +70,32 @@ const isEligible = ( event ) => {
 	const slotID = slot.getSlotElementId();
 
 	// Prevent a refresh if the ad has a blacklisted advertiser ID.
-	if ( 'undefined' !== typeof advertiserIds[ slotInfo.advertiserId ] ) {
+	if (typeof advertiserIds[slotInfo.advertiserId] !== 'undefined') {
 		return false;
 	}
 
 	// Prevent a refresh if the line item ID is blacklisted.
-	if ( 'undefined' !== typeof lineItemIds[slotInfo.lineItemId] ) {
+	if (typeof lineItemIds[slotInfo.lineItemId] !== 'undefined') {
 		return false;
 	}
 
 	// Fluid returns 0x0 - ensure fluid is supported on the unit too.
-	if ( '0,0' === slotSize && slotSizes.includes( 'fluid' ) ) {
+	if (slotSize === '0,0' && slotSizes.includes('fluid')) {
 		slotSize = 'fluid';
 	}
 
 	// Prevent a refresh if the slot size is blacklisted.
-	if ( sizesToExclude.includes( slotSize ) ) {
+	if (sizesToExclude.includes(slotSize)) {
 		return false;
 	}
 
 	// Prevent a refresh if the slot ID is blacklisted.
-	if ( 'undefined' !== typeof slotIdsToExclude[ slotID ] ) {
+	if (typeof slotIdsToExclude[slotID] !== 'undefined') {
 		return false;
 	}
 
 	// Enforce limit on maximum number of refreshes per slot.
-	if ( 'undefined' !== typeof adsData[ slotID ] &&
-			adsData[ slotID ].renderCount >= maximumRefreshes ) {
+	if (typeof adsData[slotID] !== 'undefined' && adsData[slotID].renderCount >= maximumRefreshes) {
 		return false;
 	}
 
@@ -185,31 +103,101 @@ const isEligible = ( event ) => {
 };
 
 /**
+ * Initialize the data being recorded for the slot.
+ * @param {object} event googletag.events.SlotRenderEndedEvent
+ * see: https://developers.google.com/doubleclick-gpt/reference#googletag.events.slotrenderendedevent
+ */
+const initializeSlotData = (event) => {
+	const { slot } = event;
+	const slotID = slot.getSlotElementId();
+	if (!slotID) {
+		return;
+	}
+
+	if (typeof adsData[slotID] !== 'undefined') {
+		return;
+	}
+
+	adsData[slotID] = {};
+	adsData[slotID].viewable = true;
+	// In the event the ad rendered before the plugin's event handlers were
+	// added, we need to ensure the ad data is fully populated.
+	adsData[slotID].renderCount = 1;
+	adsData[slotID].timeViewable = 0;
+	adsData[slotID].canRefresh = isEligible(event);
+	adsData[slotID].slotObject = slot;
+	adsData[slotID].viewability = 0;
+};
+
+/**
+ * Handle impressionViewable event.
+ * @param {object} event googletag.events.ImpressionViewableEvent
+ * see: https://developers.google.com/doubleclick-gpt/reference#googletag.events.impressionviewableevent
+ */
+const impressionViewableHandler = (event) => {
+	const slotID = event.slot.getSlotElementId();
+	if (!slotID) {
+		return;
+	}
+	if (typeof adsData[slotID] === 'undefined') {
+		initializeSlotData(event);
+	}
+	adsData[slotID].viewable = true;
+};
+
+/**
+ * Handle slotVisibilityChanged event.
+ * @param {object} event googletag.events.SlotVisibilityChangedEvent
+ * see: https://developers.google.com/doubleclick-gpt/reference#googletageventsslotvisibilitychangedevent
+ */
+const viewabilityHandler = (event) => {
+	let { inViewPercentage } = event;
+
+	if (typeof event.inViewPercentage === 'undefined') {
+		inViewPercentage = 100;
+	}
+
+	const slotID = event.slot.getSlotElementId();
+	if (typeof adsData[slotID] === 'undefined') {
+		initializeSlotData(event);
+	}
+
+	adsData[slotID].viewability = inViewPercentage;
+};
+
+/**
+ * Handle slotRenderEnded event.
+ * @param {object} event googletag.events.SlotRenderEndedEvent
+ * see: https://developers.google.com/doubleclick-gpt/reference#googletag.events.slotrenderendedevent
+ */
+const slotRenderEndedHandler = (event) => {
+	const slotID = event.slot.getSlotElementId();
+	if (!slotID) {
+		return;
+	}
+	if (typeof adsData[slotID] === 'undefined') {
+		initializeSlotData(event);
+	} else {
+		adsData[slotID].renderCount += 1;
+		adsData[slotID].timeViewable = 0;
+		adsData[slotID].viewable = false;
+		adsData[slotID].canRefresh = isEligible(event);
+	}
+};
+
+/**
  * Add event listeners for viewability.
  */
 const viewabilityListener = () => {
-	googletag.pubads().addEventListener( 'impressionViewable', impressionViewableHandler );
-	googletag.pubads().addEventListener( 'slotVisibilityChanged', viewabilityHandler );
-	googletag.pubads().addEventListener( 'slotRenderEnded', slotRenderEndedHandler );
+	googletag.pubads().addEventListener('impressionViewable', impressionViewableHandler);
+	googletag.pubads().addEventListener('slotVisibilityChanged', viewabilityHandler);
+	googletag.pubads().addEventListener('slotRenderEnded', slotRenderEndedHandler);
 };
 
 /**
  * Setup detection of whether the browser is focused.
  */
 const setupBrowserFocusDetection = () => {
-	if ( 'undefined' !== typeof document.hidden ) {
-		browserFocus = ! document.hidden;
-		document.addEventListener( 'visibilitychange', function() {
-			browserFocus = ! document.hidden;
-		} );
-	} else if ( 'undefined' !== typeof document.onfocusin ) {
-		document.onfocusin = indicatePageIsFocused;
-		document.onfocusout = indicatePageIsNotFocused;
-	} else {
-		window.onfocus = indicatePageIsFocused;
-		window.onblur = indicatePageIsNotFocused;
-	}
-
 	/**
 	 * Set flag showing the page is in focus within the browser.
 	 */
@@ -223,6 +211,19 @@ const setupBrowserFocusDetection = () => {
 	function indicatePageIsNotFocused() {
 		browserFocus = false;
 	}
+
+	if (typeof document.hidden !== 'undefined') {
+		browserFocus = !document.hidden;
+		document.addEventListener('visibilitychange', function () {
+			browserFocus = !document.hidden;
+		});
+	} else if (typeof document.onfocusin !== 'undefined') {
+		document.onfocusin = indicatePageIsFocused;
+		document.onfocusout = indicatePageIsNotFocused;
+	} else {
+		window.onfocus = indicatePageIsFocused;
+		window.onblur = indicatePageIsNotFocused;
+	}
 };
 
 /**
@@ -233,22 +234,22 @@ const init = () => {
 	let tries = 0;
 
 	// Check if GPT API is ready. If not, try again.
-	if ( ! googletag || ! googletag.apiReady ) {
-		const timeout = window.setTimeout( () => {
-			if ( tries <= timesToTry ) {
+	if (!googletag || !googletag.apiReady) {
+		const timeout = window.setTimeout(() => {
+			if (tries <= timesToTry) {
 				tries++;
 				init();
 			}
 
-			window.clearTimeout( timeout );
-		}, 100 );
+			window.clearTimeout(timeout);
+		}, 100);
 
 		return;
 	}
 
 	viewabilityListener();
 	setupBrowserFocusDetection();
-	setTimeout( checkForAdsReadyToRefresh, 500 );
+	setTimeout(checkForAdsReadyToRefresh, 500);
 };
 
 export default init;
--- dist/js/frontend.asset.php
+++ dist/js/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '6db53942b2c263f77eb4');
+<?php return array('dependencies' => array(), 'version' => 'bc3ab2b338f91b48db3f');
