3075f5841ad2529fa76b3d0d65533bd741347826
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2025-03-17 17:47:08 +0530

    remove null core version

--- tests/bin/set-core-version.js
+++ tests/bin/set-core-version.js
@@ -1,31 +1,46 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const { exit } = require("process");
+const fs = require( 'fs' );
 
-const path = `${process.cwd()}/.wp-env.override.json`;
+const path = `${ process.cwd() }/.wp-env.override.json`;
 
-let config = fs.existsSync(path) ? require(path) : {};
+let config = fs.existsSync( path ) ? require( path ) : { plugins: ['.'] };
 
-const args = process.argv.slice(2);
+const args = {};
+process.argv
+    .slice(2, process.argv.length)
+    .forEach( arg => {
+        if (arg.slice(0,2) === '--') {
+            const param = arg.split('=');
+            const paramName = param[0].slice(2,param[0].length);
+            const paramValue = param.length > 1 ? param[1] : true;
+            args[paramName] = paramValue;
+        }
+    });
 
-if (args.length == 0) exit(0);
-
-if (args[0] == "latest") {
-  if (fs.existsSync(path)) {
-    fs.unlinkSync(path);
-  }
-  exit(0);
+if ( ! args.core && ! args.plugins ) {
+    return;
 }
 
-config.core = args[0];
+if ( 'latest' === args.core ) {
+    delete args.core;
+}
 
-if (!config.core.match(/^WordPress\/WordPress\#/)) {
-  config.core = "WordPress/WordPress#" + config.core;
+if( Object.keys(args).length === 0 ) {
+    return;
+}
+
+if ( args.plugins ) {
+    args.plugins = args.plugins.split(',');
+}
+
+config = {
+    ...config,
+    ...args,
 }
 
 try {
-  fs.writeFileSync(path, JSON.stringify(config));
-} catch (err) {
-  console.error(err);
+   fs.writeFileSync( path, JSON.stringify( config ) );
+} catch ( err ) {
+    console.error( err );
 }
