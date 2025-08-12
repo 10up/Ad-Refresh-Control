7d51ab9125020bc273c8df44d2c6775c2fffb3d1
Sudip Dadhaniya <61381229+sudip-d@users.noreply.github.com>
2025-08-12 13:22:48 +0530

    Resolved issues

--- tests/cypress/config.js
+++ tests/cypress/config.js
@@ -13,7 +13,7 @@ module.exports = defineConfig( {
 		setupNodeEvents( on, config ) {
 			return setBaseUrl( on, config );
 		},
-		specPattern: 'tests/cypress/e2e/**/*.test.{js,jsx,ts,tsx}',
+		specPattern: 'tests/cypress/e2e/**/*.{test,cy}.{js,jsx,ts,tsx}',
 		supportFile: 'tests/cypress/support/e2e.js',
 		defaultCommandTimeout: 20000,
 	},
